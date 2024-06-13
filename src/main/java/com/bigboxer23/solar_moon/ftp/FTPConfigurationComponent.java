1257e3cb61c45d7fc9f17656fbef1e8ce259be7e
Matt Jones <bigboxer23@gmail.com>
2024-06-13 10:15:21 -0500

    chore: bump common lib version

--- src/main/java/com/bigboxer23/solar_moon/ftp/FTPConfigurationComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/ftp/FTPConfigurationComponent.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/main/java/com/bigboxer23/solar_moon/ftp/FTPCustomerComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/ftp/FTPCustomerComponent.java
@@ -2,7 +2,6 @@ package com.bigboxer23.solar_moon.ftp;
 
 import com.bigboxer23.solar_moon.customer.CustomerComponent;
 import com.bigboxer23.solar_moon.data.Customer;
-
 import java.util.List;
 import org.springframework.stereotype.Component;
 
